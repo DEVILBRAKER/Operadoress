e125fd0a2e1434952964779b89185ba614c419d0
DEVILBRAKER <128807409+DEVILBRAKER@users.noreply.github.com>
2024-05-14 08:46:06 -0300

    Add files via upload

--- operadores.js
+++ operadores.js
@@ -14,3 +14,52 @@ idade += 5;
 saldoConta -= 200; 
 quantidadeProdutos *= 2; var nomeCompleto = "João Silvarqaa";
 
+
+//soma
+function soma(a, b) {
+    return a + b;
+}
+console.log(soma(5, 3)); // 8
+
+function somaTres(a, b, c) {
+    return a + b + c;
+}
+console.log(somaTres(1, 2, 3)); // 6
+
+
+//subtração
+function subtracao(a, b) {
+    return a - b;
+}
+console.log(subtracao(10, 6)); // 4
+
+function subtracaoTres(a, b, c) {
+    return a - b - c;
+}
+console.log(subtracaoTres(20, 5, 3)); // 12
+
+
+//multiplicação
+function multiplicacao(a, b) {
+    return a * b;
+}
+console.log(multiplicacao(4, 7)); // 28
+
+function multiplicacaoTres(a, b, c) {
+    return a * b * c;
+}
+console.log(multiplicacaoTres(2, 3, 4)); // 24
+
+
+//divisão
+function divisao(a, b) {
+    return a / b;
+}
+console.log(divisao(20, 5)); // 4
+
+function divisaoSequencial(a, b, c) {
+    return a / b / c;
+}
+console.log(divisaoSequencial(100, 5, 2)); // 10
+
+
